0a715a2e7f2aa1f61156e533f0c5368d1e6fa214
Alex <alex@gentian.team>
2023-06-16 16:16:03 +0100

    Add sweep and run scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+python main.py $1 --trainer configs/trainer.yaml --model configs/model.yaml --data configs/data.yaml --optimizer configs/optimizer.yaml
--- train.sh
+++
@@ -1,1 +0,0 @@
-python main.py fit --trainer configs/trainer.yaml --model configs/model.yaml --data configs/data.yaml --optimizer configs/optimizer.yaml
